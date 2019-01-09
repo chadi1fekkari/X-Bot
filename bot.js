9784cd051b39fbd2efeae660e3e21301d1a7579b
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2019-01-09 18:59:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,'X.help','Type X.help',`${client.users.size} Members`,'X.inv','By: !  SizGooo ツ#4433'];
+    var setGame = [`${client.guilds.size} Server`,'X.help','Type X.help',`${client.users.size} Members`,'X.inv','By: !  SizGooo ツ'];
     var i = -1;
     var j = 0;
     setInterval(function (){
